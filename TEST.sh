35101a3d4118041166a5228b27c68b79563bc67e
peter <42803300+peter1938@users.noreply.github.com>
2018-10-09 15:06:00 +0200

    Add files via upload
    Added Test-Shell (Test only)

---
+++ TEST.sh
@@ -0,0 +1,44 @@
+#! /bin/bash
+
+echo 'TEST ONLY'
+echo 'NO TAGBING'
+echo 'NO PULLING TO DOCKER HUB'
+
+#Default compose args
+COMPOSE_ARGS=" -f jenkins.yml -p jenkins "
+
+#Make sure old containers are gone
+sudo docker-compose $COMPOSE_ARGS stop
+sudo docker-compose $COMPOSE_ARGS rm --force -v
+
+#build the system
+sudo docker-compose $COMPOSE_ARGS build --no-cache
+sudo docker-compose $COMPOSE_ARGS up -d
+
+#Run unit tests
+sudo docker-compose $COMPOSE_ARGS run --no-deps --rm -e ENV=UNIT identidock
+ERR=$?
+
+#Run system test if unit tests passed
+if [ $ERR -eq 0 ]; then
+  IP=$(sudo docker inspect -f {{.NetworkSettings.IPAddress}} jenkins_identidock_1)
+  CODE=$(curl -sL -w "%{http_code}" $IP:9090/monster/bla -o /dev/null) || true
+  if [ $CODE -eq 200 ]; then
+    echo "Test passed"
+    HASH=$(git rev-parse --short HEAD)
+    echo 'GIT_HASH = ' $HASH
+  else
+    echo "Site returned " $CODE
+    ERR=1
+  fi
+fi
+
+#Pull down the system
+sudo docker-compose $COMPOSE_ARGS stop
+sudo docker-compose $COMPOSE_ARGS rm --force -v
+if [ $ERR -eq 0 ]; then
+  echo 'SUCCESS'
+else 
+  echo 'FAILURE = ' $ERR
+fi
+return $ERR
